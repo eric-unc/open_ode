64f9535ab352b289209e4cb8cccba3eda755ae2c
Eric Schneider <u1643364@live.unc.edu>
2020-08-08 20:27:29 -0400

    Get closer to fixing euler's

--- lib/odes.rb
+++ lib/odes.rb
@@ -7,13 +7,15 @@ module ODE
 	def forward_euler(func, t0, y0, tf, n)
 		dt = (tf - t0)/(n - 1)
 		t = linspace(t0, tf, n)
-		y_app = Array.new(n)
+		y_app = Array.new(n, 0.0)
 		y_app[0] = y0
 
 		(1..(n - 1)).each do |i|
+			#puts dt*func.call(t[i], y_app[i - 1])
+			puts dt*(t[i] ** 2)
 			y_app[i] = y_app[i - 1] + dt*func.call(t[i], y_app[i - 1])
 		end
 
-		[t, t]
+		[t, y_app]
 	end
 end
